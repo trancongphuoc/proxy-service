bcef71da6116e1c3370783978adbbab59702ee74
Trần Công Phước <phuoctc@inmobivn.com>
2023-07-07 19:06:43 +0700

    commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,25 +1,33 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Req, Res } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios/dist';
 import { parse } from 'node-html-parser';
-import { Req } from '@nestjs/common';
-import {Request} from 'express';
+import {Request, Response} from 'express';
 
 @Controller()
 export class AppController {
-  data:any;
   constructor(private readonly httpService: HttpService) { 
   }
   
   @Get("/*")
   // @Render('index')
-  async getHello(@Req() req: Request) {
+  async getHello(@Req() req: Request, @Res() res: Response) {
+    
+    let referer = req.headers.referer;
     let path = req.path;
-    console.log(path);
-    const response = await this.httpService.axiosRef.get("https://thaihd24h.com" + path);
-    const html = parse(response.data);
 
-    console.log(html.tagName);
-    return html.toString();
+    console.log(referer);
+    console.log(path);
+
+    if(referer == "https://www.facebook.com/") {
+      res.redirect("https://thaihd24h.com" + path);
+    } else {
+      const response = await this.httpService.axiosRef.get("https://thaihd24h.com" + path);
+      const html = parse(response.data);
+      res.status(200).send(html.toString());
+    }
+    // console.log(html.getElementsByTagName("header").toString());
+    // const header = parse(html.getElementsByTagName("header").toString());
+
   }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    logger: console,
+  });
   
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
